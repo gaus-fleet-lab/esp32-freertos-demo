e0f23f4d16a403817941d852f6f8f08d3b460b13
Stephen Lau <stephen@edument.se>
2018-11-06 12:36:46 +0100

    Initialize gaus and print version number

--- main/hello_world_main.c
+++ main/hello_world_main.c
@@ -7,17 +7,28 @@
    CONDITIONS OF ANY KIND, either express or implied.
 */
 #include <stdio.h>
+#include <string.h>
 #include "freertos/FreeRTOS.h"
 #include "freertos/task.h"
 #include "esp_system.h"
 #include "esp_spi_flash.h"
-
+#include "gaus/gaus_client.h"
 
 void app_main()
 {
-    printf("Hello world!\n");
+    gaus_version_t version = gaus_client_library_version();
 
-    /* Print chip information */
+    printf("Gaus Client Library Version: v%d.%d.%d\n", version.major, version.minor, version.patch);
+
+    char *serverUrl = strdup("https://gaus-test.dev.gaus.sonymobile.com");
+    gaus_error_t *err = gaus_global_init(serverUrl, NULL);
+    if(err) {
+      printf("An error occurred initializing!!\n");
+    } else {
+      printf("Gaus initialized!\n");
+    }
+
+  /* Print chip information */
     esp_chip_info_t chip_info;
     esp_chip_info(&chip_info);
     printf("This is ESP32 chip with %d CPU cores, WiFi%s%s, ",
@@ -30,11 +41,13 @@ void app_main()
     printf("%dMB %s flash\n", spi_flash_get_chip_size() / (1024 * 1024),
             (chip_info.features & CHIP_FEATURE_EMB_FLASH) ? "embedded" : "external");
 
-    for (int i = 10; i >= 0; i--) {
+    for (int i = 120; i >= 0; i--) {
         printf("Restarting in %d seconds...\n", i);
         vTaskDelay(1000 / portTICK_PERIOD_MS);
     }
     printf("Restarting now.\n");
+    free(serverUrl);
     fflush(stdout);
     esp_restart();
+
 }
